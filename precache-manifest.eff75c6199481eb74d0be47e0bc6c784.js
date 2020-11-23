6092cab293de747f321facf7d52498555034e23b
shpinata <nata.shapar2011@yandex.ru>
2020-11-23 20:55:41 +0300

    Updates

---
+++ precache-manifest.eff75c6199481eb74d0be47e0bc6c784.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "897bb6886b1712cb0759",
+    "url": "/tainted-grail/static/css/main.bdd226f9.chunk.css"
+  },
+  {
+    "revision": "897bb6886b1712cb0759",
+    "url": "/tainted-grail/static/js/main.8f955eac.chunk.js"
+  },
+  {
+    "revision": "7323b92ac1771c6e5071",
+    "url": "/tainted-grail/static/js/runtime~main.719c1334.js"
+  },
+  {
+    "revision": "2a7338125200a4384dda",
+    "url": "/tainted-grail/static/css/2.6d6959bf.chunk.css"
+  },
+  {
+    "revision": "2a7338125200a4384dda",
+    "url": "/tainted-grail/static/js/2.b4133869.chunk.js"
+  },
+  {
+    "revision": "484c96032bdb5b5602a4bc586f7ebd30",
+    "url": "/tainted-grail/static/media/logo.484c9603.png"
+  },
+  {
+    "revision": "88b47ce232cf103fc49277ce29a028ad",
+    "url": "/tainted-grail/static/media/MasonChronicles.88b47ce2.eot"
+  },
+  {
+    "revision": "420a00b9a7b3178e61123421146782e2",
+    "url": "/tainted-grail/static/media/MasonChronicles.420a00b9.ttf"
+  },
+  {
+    "revision": "395c8013fbc34c150fabd05753f5511d",
+    "url": "/tainted-grail/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/tainted-grail/precache-manifest.eff75c6199481eb74d0be47e0bc6c784.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/tainted-grail/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
